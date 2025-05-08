0d2a83edf0326f17de2312dfd6eeea57a6b6ef6a
qkd0863 <qkd7183@naver.com>
2025-05-08 23:51:26 +0900

    init, onDraw 정의

--- app/src/main/java/com/example/nom/GameView.java
+++ app/src/main/java/com/example/nom/GameView.java
@@ -1,17 +1,28 @@
 package com.example.nom;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
     public GameView(Context context) {
         super(context);
+        init();
     }
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public void init() {
+
+    }
+
+    public void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
