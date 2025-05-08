0d81cfc3c3873fc178d3669d12be539d1ab31efc
qkd0863 <qkd7183@naver.com>
2025-05-08 23:36:48 +0900

    GameView 생성, 생성자 작성

---
+++ app/src/main/java/com/example/nom/GameView.java
@@ -0,0 +1,10 @@
+package com.example.nom;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+}
