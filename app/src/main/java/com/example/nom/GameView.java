3f54abe4b14b0d488afcde93de3376f27aa6dc77
qkd0863 <qkd7183@naver.com>
2025-05-08 23:52:59 +0900

    사각형 그리기

--- app/src/main/java/com/example/nom/GameView.java
+++ app/src/main/java/com/example/nom/GameView.java
@@ -2,6 +2,10 @@ package com.example.nom;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,6 +13,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private static final float SCREEN_WIDTH = 900f;
+    private static final float SCREEN_HEIGHT = 1600f;
+    private final Matrix transformMatrix = new Matrix();
+    private final RectF borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    private final Paint borderPaint = new Paint();
+
     public GameView(Context context) {
         super(context);
         init();
@@ -19,10 +29,13 @@ public class GameView extends View {
     }
 
     public void init() {
-
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(10.0f);
+        borderPaint.setColor(Color.RED);
     }
 
     public void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawRect(borderRect, borderPaint);
     }
 }
