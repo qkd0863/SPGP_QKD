e560eef87a979bc72c1fa0bb5aa74f181db537ad
qkd0863 <qkd7183@naver.com>
2025-05-09 23:47:17 +0900

    bitmap 로드를 class에서 하도록 변경

--- app/src/main/java/com/example/nom/Fighter.java
+++ app/src/main/java/com/example/nom/Fighter.java
@@ -1,6 +1,8 @@
 package com.example.nom;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.RectF;
 
@@ -8,8 +10,9 @@ public class Fighter implements IGameObject {
     private final Bitmap bitmap;
     private final RectF dstRect = new RectF();
 
-    public Fighter(Bitmap bitmap) {
-        this.bitmap = bitmap;
+    public Fighter() {
+        Resources res = GameView.view.getResources();
+        bitmap = BitmapFactory.decodeResource(res, R.mipmap.plane_240);
         float cx = 500f, y = 1200f;
         float r = 125f;
         dstRect.set(cx-r, y, cx+r, y+2*r);
