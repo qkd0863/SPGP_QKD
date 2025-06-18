09c3a1a1b2fbd173f0fc89a66aebc20c064d1cc4
qkd0863 <qkd7183@naver.com>
2025-06-18 22:17:22 +0900

    메인화면 이미지추가

--- app/src/main/java/com/example/nom/Nom/game/GameOverScene.java
+++ app/src/main/java/com/example/nom/Nom/game/GameOverScene.java
@@ -18,11 +18,11 @@ public class GameOverScene extends Scene {
     }
 
     @Override
-    public void update() { /* 정지 화면이므로 로직 없음 */ }
+    public void update() {  }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawColor(Color.argb(200, 0, 0, 0)); // 반투명 배경
+        canvas.drawColor(Color.argb(200, 0, 0, 0));
         canvas.drawText("GAME OVER", Metrics.width / 2f, Metrics.height / 2f, textPaint);
         canvas.drawText("Tap to restart", Metrics.width / 2f, Metrics.height / 2f + 180, textPaint);
     }
@@ -30,8 +30,8 @@ public class GameOverScene extends Scene {
     @Override
     public boolean onTouchEvent(android.view.MotionEvent e) {
         if (e.getAction() == android.view.MotionEvent.ACTION_DOWN) {
-            GameView.view.popAllScenes();          // 스택 정리
-            GameView.view.pushScene(new MainScene()); // 게임 재시작
+            GameView.view.popAllScenes();
+            GameView.view.pushScene(new MainScene());
         }
         return true;
     }
